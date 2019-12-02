fab068e4146dc061e7d79d9a353229af6b602b09
jan <declercqjan@gmail.com>
2019-12-02 11:02:35 +0100

    pauze

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "hallo weather app"
-?>
+// $myvar = "This is my variable";
+// echo $myvar;
+$name = "jan";
+$greetings = "hello";
+// punt nodig voor string te concaten
+$greet_name = $greetings . $name;
+echo $greet_name;
+?>;
